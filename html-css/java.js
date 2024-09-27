43dc7a5c9b325e39c8541718c073193a92fae920
Dreaming <abraao.neto@souunit.com.br>
2024-09-27 00:13:59 -0300

    Enxugando o código realocando arquivos css num único arquivo

--- html-css/java.js
+++ html-css/java.js
@@ -1,10 +1,8 @@
-document.getElementById('profile-button').addEventListener('click', function() {
+document.getElementById('profile-button').addEventListener('click', function () {
     const menu = document.getElementById('profile-menu');
     menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
 });
-
-// Opcional: Fechar o menu ao clicar fora dele
-document.addEventListener('click', function(event) {
+document.addEventListener('click', function (event) {
     const menu = document.getElementById('profile-menu');
     const button = document.getElementById('profile-button');
     if (!menu.contains(event.target) && !button.contains(event.target)) {
@@ -22,36 +20,32 @@ function toggleAnswer(answerId) {
 function startDownload(progressId, button) {
     let progressBar = document.getElementById(progressId);
     let width = 0;
-    button.disabled = true; // Desativa o botão enquanto o download está ativo
+    button.disabled = true; 
 
-    let interval = setInterval(function() {
-      if (width >= 100) {
-        clearInterval(interval);
-        button.innerHTML = "Concluído";
-        button.style.backgroundColor = "#4CAF50";
-      } else {
-        width++;
-        progressBar.style.width = width + '%';
-      }
-    }, 100); // Simula o progresso de download
-  }
-
-  function cancelDownload(progressId) {
+    let interval = setInterval(function () {
+        if (width >= 100) {
+            clearInterval(interval);
+            button.innerHTML = "Concluído";
+            button.style.backgroundColor = "#4CAF50";
+        } else {
+            width++;
+            progressBar.style.width = width + '%';
+        }
+    }, 100);
+}
+function cancelDownload(progressId) {
     let progressBar = document.getElementById(progressId);
-    progressBar.style.width = '0'; // Reseta a barra de progresso
+    progressBar.style.width = '0'; 
     alert("Download cancelado.");
-  }
-    
-  function previewImage(event) {
+}
+function previewImage(event) {
     var reader = new FileReader();
-    reader.onload = function() {
+    reader.onload = function () {
         var output = document.getElementById('profileImage');
         output.src = reader.result;
     }
     reader.readAsDataURL(event.target.files[0]);
 }
-
-
 function saveProfile() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
@@ -64,10 +58,8 @@ function saveProfile() {
         alert("Por favor, preencha os campos necessários.");
     }
 }
-
-
 function logout() {
     alert("Você foi deslogado.");
-    
-    window.location.href = 'login.html'; 
+
+    window.location.href = 'login.html';
 }
